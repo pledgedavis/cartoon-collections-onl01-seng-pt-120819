d76b7aaac7b19b655d8aa20ba3a268ce9979da73
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 21:47:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,3 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"] 
   array.any? do |string|
        cheese_types.first
-  end
-if 
-  puts false 
-end
