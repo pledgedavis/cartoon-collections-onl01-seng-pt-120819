708bea713420715d4a9f97426bf4ad1b3483194e
pledgedavis <pledgedavis14@gmail.com>
2019-12-20 20:18:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index do |counter|
-    puts "#{counter.} #{counter}"
+  array.each_with_index do |counter, index|
+    puts "#{index} #{counter}"
     
 end
 end
